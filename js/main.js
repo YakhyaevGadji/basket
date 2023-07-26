3fdcbff8998fcb7d6fa024f09587bd830f733049
gadji <gadji180105@gmail.com>
2023-07-26 18:40:41 +0300

    add task

--- js/main.js
+++ js/main.js
@@ -1,35 +1,58 @@
-const dataActions = document.querySelectorAll('[data-action]');
+const dataCarts = document.querySelectorAll('[data-cart]');
+const cartWrapper = document.querySelector('.cart-wrapper');
+const cardsBody = document.querySelectorAll('.basket__parent');
 
-function forDataAction() {
-    dataActions.forEach((dataAction) => {
-        dataAction.addEventListener('click', addCounterPlus);
-        dataAction.addEventListener('click', addCounterMinus);
-    });
-}
-forDataAction();
 
-function addCounterPlus(event) {
-    const action = event.target.dataset.action;
 
-    if(action === 'plus') {
-        const counterWrapper = event.target.closest('.counter-wrapper');
-        const wrapperChildren = counterWrapper.querySelector('[data-counter]');
+dataCarts.forEach((dataCart) => {
+    dataCart.addEventListener('click', addTask);
+});
 
-        wrapperChildren.textContent++;
+
+function addTask(event) {
+    const cart = event.target.closest('.card');
+    
+    const taskElements = {
+        id: cart.dataset.id,
+        srcImg: cart.querySelector('.product-img').getAttribute('src'),
+        title: cart.querySelector('.item-title').textContent,
+        itemsInBox: cart.querySelector('[data-items-in-box').textContent,
+        counter: cart.querySelector('[data-counter]').textContent,
+        weight: cart.querySelector('.price__weight').textContent,
+        price: cart.querySelector('.price__currency').textContent
     }
+    
+    cartWrapper.insertAdjacentHTML('beforeend', taskHtml(taskElements));
+};
+
+function taskHtml(taskElements) {
+    const taskHTML = `<div class="cart-item" data-id="${taskElements.id}">
+        <div class="cart-item__top">
+            <div class="cart-item__img">
+                <img src="${taskElements.srcImg}" alt="">
+            </div>
+            <div class="cart-item__desc">
+            <div class="cart-item__title">${taskElements.title}</div>
+            <div class="cart-item__weight">${taskElements.itemsInBox} / ${taskElements.weight}</div>
+
+                <div class="cart-item__details">
+
+                    <div class="items items--small counter-wrapper">
+                        <div class="items__control" data-action="minus">-</div>
+                        <div class="items__current" data-counter="">${taskElements.counter}</div>
+                        <div class="items__control" data-action="plus">+</div>
+                    </div>
+
+                    <div class="price">
+                        <div class="price__currency">${taskElements.price}</div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </div>`;
+
+    return taskHTML;
 }
 
-function addCounterMinus(event) {
-    const action = event.target.dataset.action;
-
-    if(action === 'minus') {
-        const counterWrapper = event.target.closest('.counter-wrapper');
-        const wrapperChildren = counterWrapper.querySelector('[data-counter]');
-
-        if(wrapperChildren.textContent > 1) {
-            wrapperChildren.textContent--;
-        }else if(counterWrapper.closest('.cart-wrapper') && Number(wrapperChildren.textContent) === 1) {
-            counterWrapper.closest('.cart-item').remove();
-        }
-    }
-}
