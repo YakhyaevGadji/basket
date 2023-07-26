9d97fc1c429d7d9c5855234158118fcbd04f8c82
gadji <gadji180105@gmail.com>
2023-07-26 13:21:13 +0300

    add button plus and minus counter

---
+++ js/main.js
@@ -0,0 +1,33 @@
+const dataActions = document.querySelectorAll('[data-action]');
+
+function forDataAction() {
+    dataActions.forEach((dataAction) => {
+        dataAction.addEventListener('click', addCounterPlus);
+        dataAction.addEventListener('click', addCounterMinus);
+    });
+}
+forDataAction();
+
+function addCounterPlus(event) {
+    const action = event.target.dataset.action;
+
+    if(action === 'plus') {
+        const counterWrapper = event.target.closest('.counter-wrapper');
+        const wrapperChildren = counterWrapper.querySelector('[data-counter]');
+
+        wrapperChildren.textContent++;
+    }
+}
+
+function addCounterMinus(event) {
+    const action = event.target.dataset.action;
+
+    if(action === 'minus') {
+        const counterWrapper = event.target.closest('.counter-wrapper');
+        const wrapperChildren = counterWrapper.querySelector('[data-counter]');
+
+        if(wrapperChildren.textContent > 1) {
+            wrapperChildren.textContent--;
+        }
+    }
+}
