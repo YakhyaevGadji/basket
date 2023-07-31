1764f077c4d44542c37b70303955eb0ac4fa7110
gadji <gadji180105@gmail.com>
2023-07-31 12:55:06 +0300

    add Json tasks and remove array

--- js/main.js
+++ js/main.js
@@ -7,41 +7,79 @@ const emptyBasket = document.querySelector('[data-cart-empty]');
 parent.addEventListener('click', counterPlus);
 parent.addEventListener('click', counterMinus);
 
-let tasks = [];
-
-dataCarts.forEach((dataCart) => {
-    dataCart.addEventListener('click', addTask);
-});
+window.addEventListener('click', addTask);
 
 checkBasketInEmptyAndCalc();
 
+async function getProducts() {
+    const response = await fetch('./js/products.json');
+    const productsArray = await response.json();
+    
+    renderProducts(productsArray);
+}
+getProducts();
+
+function renderProducts(productsArray) {
+    productsArray.forEach((item) => {
+        const productsContainer = document.querySelector('#products__container');
+
+        const productHtml = `<div class="col-md-6">
+            <div class="card mb-4" data-id="0${item.id}">
+            <img class="product-img" src="img/roll/${item.imgSrc}" alt="">
+                <div class="card-body text-center">
+            <h4 class="item-title">${item.title}</h4>
+            <p><small data-items-in-box class="text-muted">${item.itemsInBox} шт.</small></p>
+
+            <div class="details-wrapper">
+                <div class="items counter-wrapper">
+                    <div class="items__control" data-action="minus">-</div>
+                    <div class="items__current" data-counter>1</div>
+                    <div class="items__control" data-action="plus">+</div>
+                </div>
+
+                <div class="price">
+                    <div class="price__weight">${item.weight}г.</div>
+                    <div class="price__currency">${item.price} ₽</div>
+                </div>
+            </div>
+
+            <button data-cart="added" type="button" class="btn btn-block btn-outline-warning">+ в корзину</button>
+
+            </div>
+            </div>
+        </div>`;
+
+        productsContainer.insertAdjacentHTML('beforeend', productHtml);
+    });
+}
+
+
 //Создание Html разметки в карзине
 function addTask(event) {
-    const cart = event.target.closest('.card');
+    if(event.target.dataset.cart === 'added') {
+        const cart = event.target.closest('.card');
     
-    const taskElements = {
-        id: cart.dataset.id,
-        srcImg: cart.querySelector('.product-img').getAttribute('src'),
-        title: cart.querySelector('.item-title').textContent,
-        itemsInBox: cart.querySelector('[data-items-in-box]').textContent,
-        counter: cart.querySelector('[data-counter]').textContent,
-        weight: cart.querySelector('.price__weight').textContent,
-        price: cart.querySelector('.price__currency').textContent
+        const taskElements = {
+            id: cart.dataset.id,
+            srcImg: cart.querySelector('.product-img').getAttribute('src'),
+            title: cart.querySelector('.item-title').textContent,
+            itemsInBox: cart.querySelector('[data-items-in-box]').textContent,
+            counter: cart.querySelector('[data-counter]').textContent,
+            weight: cart.querySelector('.price__weight').textContent,
+            price: cart.querySelector('.price__currency').textContent
+        }
+        
+        const serchTask = cartWrapper.querySelector(`[data-id="${taskElements.id}"]`);
+        
+        if(serchTask) {
+            const serhCount = serchTask.querySelector('[data-counter]');
+            serhCount.textContent = +serhCount.textContent + +taskElements.counter;
+        }else {
+            cartWrapper.insertAdjacentHTML('beforeend', taskHtml(taskElements));
+        }
+    
+        checkBasketInEmptyAndCalc();
     }
-    
-    const serchTask = cartWrapper.querySelector(`[data-id="${taskElements.id}"]`);
-    
-    if(serchTask) {
-        const serhCount = serchTask.querySelector('[data-counter]');
-        serhCount.textContent = +serhCount.textContent + +taskElements.counter;
-    }else {
-        cartWrapper.insertAdjacentHTML('beforeend', taskHtml(taskElements));
-    }
-
-    tasks.push(taskElements);
-
-    
-    checkBasketInEmptyAndCalc();
 };
 
 //Html разметка карточки
@@ -85,7 +123,8 @@ function counterPlus(event) {
         const counter = counterWrapper.querySelector('[data-counter]');
         counter.textContent++;
     }
-    checkBasketInEmptyAndCalc()
+   
+    checkBasketInEmptyAndCalc();
 }
 
 //добавление счеткчика в карзине
@@ -100,18 +139,6 @@ function counterMinus(event) {
             counter.textContent--;
         }else if(Number(counter.textContent) === 1 && counterWrapper.closest('.cart-wrapper')) {
             counterWrapper.closest('.cart-item').remove();
-
-            const cartItem = counterWrapper.closest('.cart-item').dataset.id;
-    
-            const index = tasks.findIndex((task) => {
-                if(+task.id === +cartItem) {
-                    return true;
-                }
-            });
-        
-            tasks.splice(index, 1);
-
-            
         }
         checkBasketInEmptyAndCalc();
     }
